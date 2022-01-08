cc1c339163cadade27b371f0ead308e14214dd39
nabin <nkbhaie2017@gmail.com>
2022-01-08 09:41:38 -0700

    first react component js file

---
+++ Shiba.js
@@ -0,0 +1,12 @@
+/* First react component */
+
+const Shiba = () =>{
+    return <img src="https://upload.wikimedia.org/wikipedia/commons/8/8c/Cow_%28Fleckvieh_breed%29_Oeschinensee_Slaunger_2009-07-07.jpg "/>
+}
+
+// create an instance of the Shiba component to display it in browser web page
+ReactDOM.render(<Shiba/>, document.getElementById("root"));
+
+
+
+
