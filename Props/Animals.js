1969a262830e7f0fa628bc07969352a5a63891de
nabin <nkbhaie2017@gmail.com>
2022-01-08 18:18:10 -0700

    add props to use an array with random math function

--- Props/Animals.js
+++ Props/Animals.js
@@ -11,3 +11,10 @@ const Animal = (props) => {
         </div>
     )
 }
+
+// Pass an array or bject in props
+const RandomChoice = (props) => {
+    const index = Math.floor(Math.random() * props.choices.length); // choices is an array that will be passed in App.js file
+    const choice = props.choices[index]; // this is an array
+    return <h4> {choice} </h4>
+}
